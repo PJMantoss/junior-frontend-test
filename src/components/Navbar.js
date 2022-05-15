b70ff72de3559ddaba524aead8c83d5db85f0555
PJMantoss <joelptoss@gmail.com>
2022-05-15 18:05:22 +0100

    update Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
       <Wrapper>
         <Left>Left</Left>
         <Center>
-          <Logo src={'./assets/logo.png'}>Test.</Logo>
+          <Logo src={'./assets/logo.png'} />
         </Center>
         <Right>Right</Right>
       </Wrapper>

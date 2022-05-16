8be89da609f241ce81533b83574d5a12cd64110a
PJMantoss <joelptoss@gmail.com>
2022-05-16 17:49:24 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import Home from './pages/Home';
-import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-      </Routes>
-    </>
+    <div>
+      <Home />
+    </div>
   );
 }
 

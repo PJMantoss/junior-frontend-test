3cd710f79a05119dea19514f8d384b89b03c9b2a
PJMantoss <joelptoss@gmail.com>
2022-05-16 18:01:24 +0100

    update Categories

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  height: 80px;
+  background: white;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 const Categories = () => {
+
+    const DisplayProducts = () => {};
+    
   return (
     <div>Categories</div>
   )

cdb6144b38d78669b48d2f624eac07ce42f1b1cf
PJMantoss <joelptoss@gmail.com>
2022-05-17 16:44:03 +0100

    update Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
 import Categories from './Categories';
 
 const Home = () => {
   return (
     <div>
-        <Navbar />
         <Categories />
     </div>
   )

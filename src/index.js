8afe8b712b493583ef2d729419bbb11414ad0726
PJMantoss <joelptoss@gmail.com>
2022-05-19 14:01:21 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
   uri,
-  cache
+  cache: InMemoryCache()
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

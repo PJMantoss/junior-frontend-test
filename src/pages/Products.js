074cf87b9f72d4bd9bdb53bb3e9e4da3f5383b91
PJMantoss <joelptoss@gmail.com>
2022-05-19 18:42:45 +0100

    update Products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -80,7 +80,7 @@ const Products = () => {
 
   const {error, data, loading} = useQuery(GET_PRODUCTS);
 
-  const [filter, setFilter] = useState(data);
+  const [filter, setFilter] = useState(data.categories);
 
   console.log({error, data, loading});
 

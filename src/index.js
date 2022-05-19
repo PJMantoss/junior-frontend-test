c6138193a0202b21d5aa7d2e7932430071a68dd6
PJMantoss <joelptoss@gmail.com>
2022-05-19 14:00:24 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+const client = new ApolloClient({});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

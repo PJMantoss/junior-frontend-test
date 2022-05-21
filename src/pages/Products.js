40b455af25560afe359db65cc2fa2cde77f5524a
PJMantoss <joelptoss@gmail.com>
2022-05-21 18:33:51 +0100

    update products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -144,14 +144,12 @@ const Products = () => {
                       </MenuItem>
                     </Right>
                 </Wrapper>
-                {data?.categories.products?.map((product) => (
-                  <Card key={product.id}>
-                    <ProductImage src={product.gallery[0]} alt={product.name} />
+                {data?.categories.name?.map((name) => (
+                  <Card>
+                    
                     <PrdouctInfo>
-                      <ProductName>{product.name}</ProductName>
-                      <ProductPrice>
-                        {product.prices[0].currency}{product.prices[0].amount}
-                      </ProductPrice>
+                      <ProductName>{name}</ProductName>
+                      
                     </PrdouctInfo>
                   </Card>
                 ))}

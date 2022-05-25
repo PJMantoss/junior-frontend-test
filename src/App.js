a26c8947182f203040d51187ae75af609911ae96
PJMantoss <joelptoss@gmail.com>
2022-05-25 17:23:14 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import TechProducts from './pages/TechProducts';
 import Clothes from './pages/Clothes';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Clothes />
     </div>
   );

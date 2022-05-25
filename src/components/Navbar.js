a635362ded09d9d6aa727493483a8288a8d683ef
PJMantoss <joelptoss@gmail.com>
2022-05-25 16:59:54 +0100

    update Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 // import { NavLink } from 'react-router-dom';
 
-const Container = styled.div`
-  height: 80px;
-  background: white;
-`;
+const Container = styled.div``;
 
 const Wrapper = styled.div`
   padding: 10px 20px;
@@ -37,6 +34,47 @@ const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
 `;
+
+const CategoryButton = styled.a`
+  display: inline-block;
+  background: transparent;
+  color: #1d1f22;
+  font-style: raleway;
+  font-size: 16px;
+  line-height: 19.62px;
+  font-weight: 400;
+  padding: 2px 4px 15px;
+  margin: 2px;
+  cursor: pointer;
+  text-transform: uppercase;
+  text-decoration: none;
+
+  &:active{
+    color: #5ece7b;
+    border-bottom: 2px solid #5ece7b;
+  }
+
+  &:visited{
+    color: #5ece7b;
+  }
+`;
+
+const Card = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ProductItem = styled.li`
+  list-style: none;
+`;
+
+const ProductImage = styled.img``;
+
+const PrdouctInfo = styled.div``;
+
+const CategoryName = styled.h4``;
+
 
 // const StyledNavLink = styled(NavLink)`
 //   color: #1d1f22;

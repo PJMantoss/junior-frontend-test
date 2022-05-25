dc25bc55f854de3422fd87ec87b11b9e0db842e4
PJMantoss <joelptoss@gmail.com>
2022-05-25 17:08:26 +0100

    update TechProducts component

--- src/pages/TechProducts.js
+++ src/pages/TechProducts.js
@@ -11,54 +11,6 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Left = styled.div`
-  flex: 1;
-`;
-
-const Center = styled.div`
-  flex: 1;
-  text-align: center;
-`;
-
-const Logo = styled.img`
-  width: 33px;
-  height: 30px;
-`;
-
-const Right = styled.div`
-  flex: 1;
-  text-align: right;
-`;
-
-const MenuItem = styled.div`
-  font-size: 14px;
-  cursor: pointer;
-`;
-
-const CategoryButton = styled.a`
-  display: inline-block;
-  background: transparent;
-  color: #1d1f22;
-  font-style: raleway;
-  font-size: 16px;
-  line-height: 19.62px;
-  font-weight: 400;
-  padding: 2px 4px 15px;
-  margin: 2px;
-  cursor: pointer;
-  text-transform: uppercase;
-  text-decoration: none;
-
-  &:active{
-    color: #5ece7b;
-    border-bottom: 2px solid #5ece7b;
-  }
-
-  &:visited{
-    color: #5ece7b;
-  }
-`;
-
 const Card = styled.ul`
   width: 100%;
   display: flex;

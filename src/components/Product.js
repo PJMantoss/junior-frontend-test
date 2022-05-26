ce67d68325227a6291dd139c4d122523b1745843
PJMantoss <joelptoss@gmail.com>
2022-05-26 17:48:48 +0100

    update Product

---
+++ src/components/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``;
+
+const Product = () => {
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product

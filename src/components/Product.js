d92aa018ad56731babcc799ebc9fa14aa100233e
PJMantoss <joelptoss@gmail.com>
2022-05-26 18:32:21 +0100

    update Products

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+    flex: 1;
+    margin: 5px;
+`;
 
 const ProductImage = styled.img``;
 

a66327f9809d1b379ff5577d83c3bd63758eae11
PJMantoss <joelptoss@gmail.com>
2022-05-26 18:05:58 +0100

    update Products

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,6 +3,27 @@ import styled from 'styled-components';
 
 const Container = styled.div``;
 
+const Card = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ProductItem = styled.li`
+  list-style: none;
+`;
+
+const ProductImage = styled.img``;
+
+const PrdouctInfo = styled.div``;
+
+const CategoryName = styled.h4``;
+
+const ProductName = styled.h3``;
+
+const ProductPrice = styled.p``;
+
+
 const Product = ({item}) => {
   return (
     <Container>Product</Container>
--- src/pages/AllProducts.js
+++ src/pages/AllProducts.js
@@ -5,26 +5,6 @@ import styled from 'styled-components';
 
 const Container = styled.div``;
 
-const Card = styled.ul`
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-`;
-
-const ProductItem = styled.li`
-  list-style: none;
-`;
-
-const ProductImage = styled.img``;
-
-const PrdouctInfo = styled.div``;
-
-const CategoryName = styled.h4``;
-
-const ProductName = styled.h3``;
-
-const ProductPrice = styled.p``;
-
 const GET_PRODUCT = `
   {
     category {

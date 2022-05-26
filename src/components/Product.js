1896cde5804ce800bcdbd9ca6365567997499032
PJMantoss <joelptoss@gmail.com>
2022-05-26 18:03:20 +0100

    update Products

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const Container = styled.div``;
 
-const Product = () => {
+const Product = ({item}) => {
   return (
     <Container>Product</Container>
   )

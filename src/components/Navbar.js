ff12e38aa3181471c1e6e5267b440bd89f4f93c0
PJMantoss <joelptoss@gmail.com>
2022-05-26 15:22:27 +0100

    installed icon library

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 // import { NavLink } from 'react-router-dom';
 
 const Container = styled.div``;

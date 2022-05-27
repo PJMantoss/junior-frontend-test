888227fe529722a11a5bbe830eb8de7437beefe3
PJMantoss <joelptoss@gmail.com>
2022-05-27 18:37:08 +0100

    update Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,28 +2,6 @@ import React from 'react';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import styled from 'styled-components';
 
-const Container = styled.div`
-    width: 386px;
-    height: 444px;
-    flex: 1;
-    padding: 16px;
-    box-shadow: 0px 4px 35px rgba(168, 172, 176, 0.19);
-`;
-
-const ProductImage = styled.img`
-    width: 338px;
-    height: 338px;
-    margin: 0 auto;
-`;
-
-const PrdouctInfo = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%
-    height: 100%;
-    position: relative;
-`;
-
 const Icon = styled.div`
     width: 52px;
     height: 52px;
@@ -38,6 +16,32 @@ const Icon = styled.div`
     border-radius: 50%;
     opacity: 0.0;
 `;
+
+const Container = styled.div`
+    width: 386px;
+    height: 444px;
+    flex: 1;
+    padding: 16px;
+    box-shadow: 0px 4px 35px rgba(168, 172, 176, 0.19);
+
+    &:hover ${Icon} {
+        opacity: 1.0;
+    }
+`;
+
+const ProductImage = styled.img`
+    width: 338px;
+    height: 338px;
+    margin: 0 auto;
+`;
+
+const PrdouctInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%
+    height: 100%;
+    position: relative;
+`
 
 const ProductName = styled.p`
     font-family: Raleway;

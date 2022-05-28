82b1fc8ec22f94eb025f6bd1b7eead18949bd627
PJMantoss <joelptoss@gmail.com>
2022-05-28 17:37:45 +0100

    add Product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 20px;
+`;
 
 const Product = () => {
   return (

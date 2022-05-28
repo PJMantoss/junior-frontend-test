ef922a04fc552e3fdc8489cecbd73842fa7886e8
PJMantoss <joelptoss@gmail.com>
2022-05-28 17:48:00 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import AllProducts from './pages/AllProducts';
 import TechProducts from './pages/TechProducts';
 import Clothes from './pages/Clothes';
 import Navbar from './components/Navbar';
+import Product from './pages/Product';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <AllProducts />
+      <Product />
     </div>
   );
 }

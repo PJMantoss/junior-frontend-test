a6ca8fafaba410b019ab8461d85b964ae9da20e9
PJMantoss <joelptoss@gmail.com>
2022-05-30 18:23:12 +0100

    update Cart page

---
+++ src/pages/Cart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 1300px;
+  margin: 0 auto;
+`;
+
+const Cart = () => {
+  return (
+    <div>Cart</div>
+  )
+}
+
+export default Cart

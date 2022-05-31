26d0b667fd494dd1d0345da1d95e947079a998bb
PJMantoss <joelptoss@gmail.com>
2022-05-31 11:57:21 +0100

    update Cart page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import TechProducts from './pages/TechProducts';
 import Clothes from './pages/Clothes';
 import Navbar from './components/Navbar';
 import Product from './pages/Product';
+import Cart from './pages/Cart';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Product />
+      <Cart />
     </div>
   );
 }

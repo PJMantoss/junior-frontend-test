70a444a9347a29778842189aff4acec21d494134
PJMantoss <joelptoss@gmail.com>
2022-06-19 20:42:00 +0100

    update Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';

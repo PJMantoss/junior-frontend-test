c03decea1504e199eee8ce60b8e1a24b228092fc
PJMantoss <joelptoss@gmail.com>
2022-06-20 14:07:03 +0100

    update Product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
 
 const Icon = styled.div`
     width: 52px;
@@ -82,20 +81,18 @@ const GET_PRODUCT = `
 
 const Product = ({product, price}) => {
   return (
-    <NavLink to="">
-      <Container>
-          <ProductImage src={product.gallery[0]} alt={product.name} />
-          <PrdouctInfo>
-              <Icon>
-                  <ShoppingCartOutlinedIcon style={{width:"20px", height:"20px"}} />
-              </Icon>
-              <ProductName>{product.name}</ProductName>
-              <ProductPrice>
-                  ${price}
-              </ProductPrice>
-          </PrdouctInfo>
-      </Container>
-    </NavLink>
+    <Container>
+        <ProductImage src={product.gallery[0]} alt={product.name} />
+        <PrdouctInfo>
+            <Icon>
+                <ShoppingCartOutlinedIcon style={{width:"20px", height:"20px"}} />
+            </Icon>
+            <ProductName>{product.name}</ProductName>
+            <ProductPrice>
+                ${price}
+            </ProductPrice>
+        </PrdouctInfo>
+    </Container>
   )
 }
 
